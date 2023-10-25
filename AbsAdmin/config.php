cd4f12e8af7d57e5dd22732e5f21a9e854870b6a
Jatinabstain <54092377+Jatinabstain@users.noreply.github.com>
2023-10-25 10:04:11 +0530

    Add all my files

---
+++ AbsAdmin/config.php
@@ -0,0 +1,39 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'https://www.akclinics.wlslab.com/AbsAdmin/');
+define('HTTP_CATALOG', 'https://www.akclinics.wlslab.com/');
+
+// DIR
+define('DIR_OPENCART', '/home1/wallab/akclinics.wlslab.com/');
+define('DIR_APPLICATION', DIR_OPENCART . 'AbsAdmin/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/home1/wallab/storage_akclinics/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wallab_akclinics');
+define('DB_PASSWORD', 'yVx+4YzLfgbY');
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
+define('DB_DATABASE', 'wallab_akclinics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
